d9ab31aca38bfd776918c6c63edd9ef911e129d9
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-19 09:20:56 -0700

    fix assertion copy/paste

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -7,7 +7,7 @@ class PatronTest < Minitest::Test
   def test_it_exists
     patron_1 = Patron.new("Bob", 20)
 
-    assert_instance_of Exhibit, exhibit
+    assert_instance_of Patron, patron_1
   end
 
 end

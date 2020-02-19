8bfa6577f34f865d3e38c90ad5c079ec429c7685
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-19 09:13:01 -0700

    add name test

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -11,4 +11,10 @@ class ExhibitTest < Minitest::Test
     assert_instance_of Exhibit, exhibit
   end
 
+  def test_it_returns_name
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_equal "Gems and Minerals", exhibit.name
+  end
+  
 end

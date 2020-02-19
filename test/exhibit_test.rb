3e6be3d54c14608aee0364b85b60c05a0a1c8d00
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-19 09:07:24 -0700

    finish test file and exists test

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,13 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
+require './lib/patron'
 
 class ExhibitTest < Minitest::Test
 
   def test_it_exists
-    exhibit = Exhibit.new
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
     assert_instance of Exhibit, exhibit
   end
-  
+
 end
